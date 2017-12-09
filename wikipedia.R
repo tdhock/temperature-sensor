e1eada180e0af52f07bc340d51500cc5b24b66bf
Toby Dylan Hocking <tdhock5@gmail.com>
2017-12-09 11:46:32 -0500

    extract data from FR pages, abbrevs

--- wikipedia.R
+++ wikipedia.R
@@ -15,35 +15,46 @@ pattern <- paste0(
 to.numeric <- function(chr.vec){
   as.numeric(gsub(",", "", gsub("−", "-", chr.vec)))
 }
-url.vec <- c(
-  ##Tustin="https://en.wikipedia.org/wiki/Tustin,_California",
-  ##Havasu="https://en.wikipedia.org/wiki/Lake_Havasu_City,_Arizona",
-  ##Berkeley="https://en.wikipedia.org/wiki/Berkeley,_California",
-  ##Santa_Cruz="https://en.wikipedia.org/wiki/Santa_Cruz,_California",
-  ##Brisbane="https://en.wikipedia.org/wiki/Brisbane",
-  ##Paris="https://en.wikipedia.org/wiki/Paris",
- ##  Tokyo="https://en.wikipedia.org/wiki/Tokyo",
- ##  Singapore="https://en.wikipedia.org/wiki/Singapore",
-  ##Hangzhou="https://en.wikipedia.org/wiki/Hangzhou",
-    ##Tehran="https://en.wikipedia.org/wiki/Tehran",
-  ##Kuwait="https://en.wikipedia.org/wiki/Kuwait_City",
- ##  Calgary="https://en.wikipedia.org/wiki/Calgary",
-  ## Toronto="https://en.wikipedia.org/wiki/Toronto",
-  ## Vancouver="https://en.wikipedia.org/wiki/Vancouver",
-  ##Halifax="https://en.wikipedia.org/wiki/Halifax,_Nova_Scotia",
-  ##Minneapolis="https://en.wikipedia.org/wiki/Minneapolis",
-  Montreal="https://en.wikipedia.org/wiki/Montreal"
-  ,Flagstaff="https://en.wikipedia.org/wiki/Flagstaff,_Arizona"
-  ,Waterloo="https://en.wikipedia.org/wiki/Waterloo,_Ontario"
-  ,San_Diego="https://en.wikipedia.org/wiki/San_Diego"
-  )
-climate.dt.list <- list()
-for(city in names(url.vec)){
-  city.html <- file.path("wikipedia", paste0(city, ".html"))
-  if(!file.exists(city.html)){
-    u <- url.vec[[city]]
-    download.file(u, city.html)
-  }
+abbrev.vec <- c(
+  Tustin="Tustin,_California",
+  Havasu="Lake_Havasu_City,_Arizona",
+  Berkeley="Berkeley,_California",
+  "Paris",
+  "Tokyo",
+  ##"Sherbrooke",
+  ## SherbrookeFR="https://fr.wikipedia.org/wiki/Sherbrooke",
+  ## Santa_Cruz="Santa_Cruz,_California",
+  ## "Brisbane",
+  ## "Singapore",
+  ## "Hangzhou",
+  ## "Tehran",
+  ## Kuwait="Kuwait_City",
+  ## "Calgary",
+  ## "Toronto",
+  ## "Vancouver",
+  ## Halifax="Halifax,_Nova_Scotia",
+  ## "Minneapolis",
+  "Montreal"
+ ,Flagstaff="Flagstaff,_Arizona"
+ ,Waterloo="Waterloo,_Ontario"
+ ,"San_Diego"
+ ,"Quebec"="https://fr.wikipedia.org/wiki/Québec_(ville)"
+)
+url.vec <- paste0(
+  ifelse(
+    grepl("^http", abbrev.vec),
+    "",
+    "https://en.wikipedia.org/wiki/"),
+  abbrev.vec)
+names(url.vec) <- ifelse(
+  names(abbrev.vec)=="",
+  abbrev.vec,
+  names(abbrev.vec))
+month.str <- c(
+  "Jan", "Feb", "Mar","Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+month <- factor(month.str, month.str)
+parser.fun.list <- list(en=function(city.html){
   tryCatch({
     df <- htmltab(city.html, which="//th[text()='Month']/ancestor::table")
   }, error=function(e){
@@ -79,17 +90,63 @@ for(city in names(url.vec)){
     is.na(row.name.mat[, "varname"]),
     row.name.vec,
     row.name.mat[, "varname"])
-  month.vec <- col.name.vec[col.indices]
   wide.dt <- data.table(
-    month=factor(month.vec, month.vec),
+    month,
     t(matrix(
       to.numeric(chr.mat), nrow(chr.mat), ncol(chr.mat),  
       dimnames=list(new.row.names))))
-  tall.dt <- melt(wide.dt, id.vars="month")
+  melt(wide.dt, id.vars="month")
+}, fr=function(city.html){
+  tryCatch({
+    df <- htmltab(city.html, which="//th[text()='Mois']/ancestor::table")
+  }, error=function(e){
+    unlink(city.html)
+    browseURL(u)
+    stop(
+      "unable to find climate data in web page ",
+      u, " -- either there is no table with climate data ",
+      "(is that a disambiguation page?), ",
+      " or we need to update the code to parse the unrecognized table")
+  })
+  row.name.vec <- df[, 1]
+  col.indices <- 2:13
+  var.trans.vec <- c(
+    "Température minimale moyenne (°C)"="Average low",
+    "Température maximale moyenne (°C)"="Average high")
+  is.found <- names(var.trans.vec) %in% row.name.vec
+  not.found <- names(var.trans.vec)[!is.found]
+  if(length(not.found)){
+    stop(
+      "need variables (",
+      paste(not.found, collapse=", "),
+      ") which are not present in data (",
+      paste(row.name.vec, collapse=", "),
+      ")")
+  }
+  chr.mat <- unname(as.matrix(df[, col.indices]))
+  corrected.mat <- matrix(
+    as.numeric(sub("−", "-", sub(",", ".", chr.mat))),
+    nrow(chr.mat), ncol(chr.mat),  
+    dimnames=list(row.name.vec))[names(var.trans.vec),]
+  rownames(corrected.mat) <- var.trans.vec
+  wide.dt <- data.table(
+    month,
+    t(corrected.mat))
+  melt(wide.dt, id.vars="month")
+})
+climate.dt.list <- list()
+for(city in names(url.vec)){
+  city.html <- file.path("wikipedia", paste0(city, ".html"))
+  u <- url.vec[[city]]
+  if(!file.exists(city.html)){
+    download.file(u, city.html)
+  }
+  language <- sub(".*//", "", sub("[.].*", "", u))
+  parser.fun <- parser.fun.list[[language]]
+  tall.dt <- parser.fun(city.html)
   climate.dt.list[[city]] <- data.frame(city, tall.dt)
 }
 climate.dt <- data.table(do.call(rbind, climate.dt.list))
-
 temp.dt <- climate.dt[variable %in% c("Average high", "Average low")]
 temp.wide <- dcast(temp.dt, city + month ~ variable)
 breaks.vec <- seq(1, 12, by=2)
